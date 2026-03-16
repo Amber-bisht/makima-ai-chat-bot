6da670413e5d14d8070cf57f7e555e1355476207
amber-bisht <bishtamber0@gmail.com>
2026-03-16 19:42:49 +0530

    fixed welcome

--- src/index.js
+++ src/index.js
@@ -110,7 +110,10 @@ async function bootstrap() {
     polling: {
       autoStart: true,
       interval: 300,
-      params: { timeout: 25 }
+      params: { 
+        timeout: 25,
+        allowed_updates: ["message", "edited_message", "channel_post", "edited_channel_post", "inline_query", "chosen_inline_result", "callback_query", "shipping_query", "pre_checkout_query", "poll", "poll_answer", "my_chat_member", "chat_member", "chat_join_request"]
+      }
     }
   });
   const botProfile = await bot.getMe();
